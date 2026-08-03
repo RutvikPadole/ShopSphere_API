40accfacc233a518aee468e161aca7cb736cdfbb
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-03 21:31:19 +0530

    Add  Authcontroller for Jwt Authentication

---
+++ ShopSphere_API/Controllers/Authcontroller.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using ShopSphere_API.Data;
+using ShopSphere_API.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ShopSphere_API.Controllers
+{
+    public class Authcontroller : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+
+        public Authcontroller (AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost ("Register")]
+        
+        public IActionResult Register(User user)
+        {
+            _context.Users.Add (user);
+            _context.SaveChanges();
+            return Ok("User Registered");
+        }
+
+        [HttpPost ("Login")]
+
+        public IActionResult Login (User loginUser)
+        {
+            var user = _context.Users
+                .FirstOrDefault(X => X.Email == loginUser.Email && X. Password == loginUser.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+            var token = GenerateToken(user);
+            return Ok(token);
+        }
+
+        private string GenerateToken (User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+
+                issuer: _config["Jwt:Isuuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                 signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
