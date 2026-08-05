89f9d87af301f3dd502c1ccc582596198854911b
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-05 19:51:26 +0530

    Add payment entities

---
+++ ShopSphere_API/Entities/Payment.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopSphere_API.Entities
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int OrderId {  get; set; }
+        public Order Order { get; set; }
+        public Decimal Amount {  get; set; }
+        public string status {  get; set; }
+        public string PaymentMethod {  get; set; }
+
+    }
+}
