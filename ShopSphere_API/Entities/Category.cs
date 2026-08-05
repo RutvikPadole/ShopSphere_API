50af4a5cded123a88e5b29d8bcae80670fc8b779
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-05 19:47:14 +0530

    Add Category entities

---
+++ ShopSphere_API/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http.Headers;
+
+namespace ShopSphere_API.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
