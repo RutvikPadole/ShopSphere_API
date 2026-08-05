6d4a87aa7232142072547d444fc93690e5cff732
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-05 19:48:04 +0530

    Add order entities

---
+++ ShopSphere_API/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopSphere_API.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId {  get; set; }
+        public User User { get; set; }
+        public  DateTime OrderDate { get; set; }
+        public ICollection<OrderItem> OrderItems   { get; set; }
+        public Payment Payment { get; set; }
+
+
+
+    }
+}
