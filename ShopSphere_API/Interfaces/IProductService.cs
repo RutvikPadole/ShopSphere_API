54207baf7c4ef3b65dd74da992b0d98d7ad6f4cc
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-08 23:28:30 +0530

    Create Interface IProductService

---
+++ ShopSphere_API/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ShopSphere_API.DTOs;
+
+namespace ShopSphere_API.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProducts();
+        Task<ProductDto> GetProductById(int id);
+        Task CreateProduct(CreateProductDto dto);
+
+
+    }
+}
