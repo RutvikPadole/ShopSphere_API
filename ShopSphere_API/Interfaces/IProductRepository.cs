eec332881970ca86d8d922e559de0744e5a1d8fd
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-08 23:27:07 +0530

    Create Interface IProductRepository

---
+++ ShopSphere_API/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopSphere_API.Entities;
+namespace ShopSphere_API.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task DeleteAsync(Product product);
+        Task SaveAync();
+
+    }
+}
