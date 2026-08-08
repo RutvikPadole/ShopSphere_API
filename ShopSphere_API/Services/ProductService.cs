6da2475ca77f8420b1acc206e55e5c3afb70a354
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-08 23:21:34 +0530

    Create ProductService

---
+++ ShopSphere_API/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using ShopSphere_API.DTOs;
+using ShopSphere_API.Interfaces;
+
+namespace ShopSphere_API.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repo;
+        public ProductService(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task <IEnumerable<ProductDto>> GetAllProducts()
+        {
+            var products = await = _repo.GetAllAsync();
+            return products.Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+            });
+        }
+    }
+}
