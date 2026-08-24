346a8709c6b904bd1f4df017b42cf6cef91efa24
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-24 17:17:05 +0530

    Update interface file

--- ShopSphere_API/Interfaces/IProductService.cs
+++ ShopSphere_API/Interfaces/IProductService.cs
@@ -5,7 +5,13 @@ namespace ShopSphere_API.Interfaces
     public interface IProductService
     {
         Task<IEnumerable<ProductDto>> GetAllProducts();
-        Task<ProductDto> GetProductById(int id);
+
+        Task<ProductDto?> GetProductById(int id);
+
         Task CreateProduct(CreateProductDto dto);
+
+        Task<bool> UpdateProduct(int id, CreateProductDto dto);
+
+        Task<bool> DeleteProduct(int id);
     }
 }
