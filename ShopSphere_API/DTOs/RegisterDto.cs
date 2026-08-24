4287a023678967291e48cd3f39211b60b595b358
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-24 17:16:19 +0530

    Add RegisterDto

---
+++ ShopSphere_API/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopSphere_API.DTOs
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
