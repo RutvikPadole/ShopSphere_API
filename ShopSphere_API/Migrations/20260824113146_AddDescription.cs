dc7abce0c20359fdebe233a872b7cb5f097c3c4a
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-24 17:17:50 +0530

    Add Description

---
+++ ShopSphere_API/Migrations/20260824113146_AddDescription.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopSphere_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_cart_users_UserId",
+                table: "cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_cartItems_products_ProductId",
+                table: "cartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderItems_products_ProductId",
+                table: "orderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_users_UserId",
+                table: "orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_products",
+                table: "products");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "products",
+                newName: "Products");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_products_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cart_Users_UserId",
+                table: "cart",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cartItems_Products_ProductId",
+                table: "cartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderItems_Products_ProductId",
+                table: "orderItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_Users_UserId",
+                table: "orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_cart_Users_UserId",
+                table: "cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_cartItems_Products_ProductId",
+                table: "cartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderItems_Products_ProductId",
+                table: "orderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_Users_UserId",
+                table: "orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "users");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "products");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "products",
+                newName: "IX_products_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_products",
+                table: "products",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cart_users_UserId",
+                table: "cart",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cartItems_products_ProductId",
+                table: "cartItems",
+                column: "ProductId",
+                principalTable: "products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderItems_products_ProductId",
+                table: "orderItems",
+                column: "ProductId",
+                principalTable: "products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_users_UserId",
+                table: "orders",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
