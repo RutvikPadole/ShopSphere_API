23af9854ba7ddfdfcfc5525e352f14062c2fa1ab
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-30 23:22:45 +0530

    Making changes in filr

--- ShopSphere_API/Services/ProductRepository.cs
+++ ShopSphere_API/Services/ProductRepository.cs
@@ -16,31 +16,37 @@ namespace ShopSphere_API.Repositories
         }
 
         public async Task<List<Product>> GetAllAsync()
+
         {
             return await _context.Products.ToListAsync();
         }
 
         public async Task<Product?> GetByIdAsync(int id)
+
         {
             return await _context.Products.FindAsync(id);
         }
 
         public async Task AddAsync(Product product)
+
         {
             await _context.Products.AddAsync(product);
         }
 
         public void Update(Product product)
+
         {
             _context.Products.Update(product);
         }
 
         public void Delete(Product product)
+
         {
             _context.Products.Remove(product);
         }
 
         public async Task SaveAsync()
+
         {
             await _context.SaveChangesAsync();
         }
