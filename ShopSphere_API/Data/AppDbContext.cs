ffce134f607ba1483547e5b1402d63fd8fd7b103
RutvikPadole <rutvikpadole23@gmail.com>
2026-08-30 23:26:25 +0530

    Update AppDbContext

--- ShopSphere_API/Data/AppDbContext.cs
+++ ShopSphere_API/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace ShopSphere_API.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+
         }
 
         public DbSet<User> Users { get; set; }
