ecaf706c7f275df941ba0b7475fbd2cea889faae
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-13 20:22:48 +0530

    Create Mapping Profile

---
+++ ShopSphere_API/Mapping/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ShopSphere_API.DTOs;
+using ShopSphere_API.Entities;
+
+
+namespace ShopSphere_API.Mapping
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>();
+
+            CreateMap<CreateProductDto, Product>();
+        }
+       
+    }
+}
