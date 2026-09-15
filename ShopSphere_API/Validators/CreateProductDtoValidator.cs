d794e30582a9db11d74b75d14367d4d882125939
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-15 18:45:08 +0530

    Add FluentValidation

---
+++ ShopSphere_API/Validators/CreateProductDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ShopSphere_API.DTOs;
+
+namespace ShopSphere_API.Validators
+{
+    public class CreateProductDtoValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Product name is required");
+
+            RuleFor(x => x.Price)
+                .NotEmpty
+                .with
+        }
+
+    }
+}
