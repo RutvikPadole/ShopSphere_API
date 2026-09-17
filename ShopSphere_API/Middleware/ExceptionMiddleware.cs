9bc4fa9d98808e1917b3e30e812e566700f692af
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-18 00:38:20 +0530

    Add Middleware

--- ShopSphere_API/Middleware/ExceptionMiddleware.cs
+++ ShopSphere_API/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,11 @@
-﻿namespace ShopSphere_API.Middleware
+﻿using Microsoft.AspNetCore.Http;
+using ShopSphere_API.Entities;
+namespace ShopSphere_API.Middleware
 {
     public class ExceptionMiddleware
     {
+
     }
 }
+
+
