cd40deb7eb8119efe8b0752aa8c10ef268f45c46
RutvikPadole <rutvikpadole23@gmail.com>
2026-09-19 23:25:56 +0530

    Update Middleware

--- ShopSphere_API/Middleware/ExceptionMiddleware.cs
+++ ShopSphere_API/Middleware/ExceptionMiddleware.cs
@@ -5,7 +5,6 @@ namespace ShopSphere_API.Middleware
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
-
         public ExceptionMiddleware(RequestDelegate next)
         {
             _next = next;
